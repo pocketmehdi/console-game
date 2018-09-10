ed987980cfa5cf20a4cc11924c93cf9e01b8dff3
tania <pocketmehdi@gmail.com>
2018-09-10 20:28:33 +0300

    Add code quality checking
    Connect travis, codeClimate, eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { getUserName } from '..';
 
-console.log("Welcome to the Brain Games!");
+import getUserName from '..';
+
+console.log('Welcome to the Brain Games!');
 getUserName();
