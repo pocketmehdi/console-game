1361be35b3ea536fc1b12ce4c87bbed6ba84b9ae
tania <pocketmehdi@gmail.com>
2018-09-11 20:51:17 +0300

    Add brain even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { evenGame } from '..';
+
+console.log('Welcome to the Brain Games! \nAnswer "yes" if number even otherwise answer "no".\n');
+evenGame();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@ import readlineSync from 'readline-sync';
 
 const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!\n`);
+  return userName;
 };
 
-export default getUserName;
+const getRandomNumber = () => Math.round(Math.random() * 100);
+const getCorrectAnswer = (number) => {
+  const answer = number % 2 === 0 ? 'yes' : 'no';
+  return answer;
+};
+
+const evenGame = () => {
+  const userName = getUserName();
+
+  const singleRound = (stepNumber) => {
+    if (stepNumber > 3) {
+      console.log(`Congratulations, ${userName}!`);
+      return;
+    }
+
+    const randomNumber = getRandomNumber();
+    const correctAnswer = getCorrectAnswer(randomNumber);
+    const answer = readlineSync.question(`Question: ${randomNumber} \nYour answer: `);
+    if (answer.toLowerCase() !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}' \nLet's try again, ${userName}!`);
+    } else {
+      console.log('Correct!');
+      singleRound(stepNumber + 1);
+    }
+  };
+
+  singleRound(1);
+};
+
+export { getUserName, evenGame };
